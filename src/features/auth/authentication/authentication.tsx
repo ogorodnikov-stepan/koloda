1cda9eaeddb00b58f3c6a35afd92857977e91c68
ogorodnikov-stepan <30865856+ogorodnikov-stepan@users.noreply.github.com>
2024-01-12 18:17:10 +0300

    Update authentication components

--- src/features/auth/authentication/authentication.tsx
+++ src/features/auth/authentication/authentication.tsx
@@ -1,20 +1,24 @@
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import urls from 'features/app/routing/urls';
 import { Tabs, TabsHeader, TabHeader, TabContent } from 'features/app/ui/tabs/tabs';
 import Feature from 'features/app/ui/feature/feature';
 import Signup from './signup';
 import Login from './login';
 import './authentication.scss';
 
+const PREFIX = 'auth:authentication';
+
 export default function Auth() {
-  const { t } = useTranslation('auth');
+  const { t } = useTranslation();
 
   return (
-    <Feature className="authentication">
+    <Feature entity="authentication">
       <Tabs>
         <Feature.Header>
           <TabsHeader>
-            <TabHeader>{t('authentication.login.title')}</TabHeader>
-            <TabHeader disabled>{t('authentication.signup.title')}</TabHeader>
+            <TabHeader>{t(`${PREFIX}.login.title`)}</TabHeader>
+            <TabHeader disabled>{t(`${PREFIX}.signup.title`)}</TabHeader>
           </TabsHeader>
         </Feature.Header>
         <Feature.Content>
@@ -22,6 +26,14 @@ export default function Auth() {
           <TabContent><Signup /></TabContent>
         </Feature.Content>
       </Tabs>
+      <Feature.Footer>
+        <Link
+          className="authentication__demo-link"
+          to={urls.home({ isDemo: true })}
+        >
+          {t(`${PREFIX}.demo`)}
+        </Link>
+      </Feature.Footer>
     </Feature>
   );
 }
