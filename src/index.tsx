ecceaa761124d7ed09c67cc67ca98bb1ab6cf2c7
ogorodnikov-stepan <30865856+ogorodnikov-stepan@users.noreply.github.com>
2024-01-16 22:03:38 +0300

    Add lazy imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,13 @@
-import { StrictMode } from 'react';
+import { lazy, Suspense, StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
-import queryClient from 'config/query';
-import 'config/i18n';
-import App from 'features/app/app';
+
+const App = lazy(() => import('features/app/app'));
 
 ReactDOM.render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <App />
-      </Router>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  </StrictMode>,
+  <Suspense fallback={null}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Suspense>,
   document.getElementById('app'),
 );
